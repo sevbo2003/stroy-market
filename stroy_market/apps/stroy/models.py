cde1f927ec4e4aef41346b521dc58727a783d232
Abdusamad Malikov <sevbofx@gmail.com>
2023-02-22 14:43:50 +0500

    `Product` model created

--- stroy_market/apps/stroy/models.py
+++ stroy_market/apps/stroy/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.utils.text import slugify
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Category(models.Model):
@@ -40,3 +43,49 @@ class SubCategory(models.Model):
         if not self.slug:
             self.slug = slugify(self.name)
         super(SubCategory, self).save(*args, **kwargs)
+
+
+class Product(models.Model):
+    TYPE = (
+        ('dona', 'шт'),
+        ('m2', 'м2'),
+    )
+    name = models.CharField(max_length=125)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    count = models.IntegerField()
+    weight = models.IntegerField(default=0)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+    product_type = models.CharField(max_length=125, choices=TYPE, default='m2')
+    control = models.CharField(max_length=125)
+    purpose = models.CharField(max_length=125)
+    material = models.CharField(max_length=125)
+    xususiyatlari = models.CharField(max_length=125)
+    brand = models.CharField(max_length=125)
+    sotuvchi = models.CharField(max_length=125)
+    description = models.TextField(blank=True, null=True)
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product')
+
+    def __str__(self):
+        return self.product.name
+
+
+class Size(models.Model):
+    name = models.CharField(max_length=125)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+
+class Color(models.Model):
+    name = models.CharField(max_length=125)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
