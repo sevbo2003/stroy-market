31618a8bcce105436070959418cff70b8bafd722
Abdusamad Malikov <sevbofx@gmail.com>
2023-02-22 13:44:12 +0500

    `Category` models created

--- stroy_market/apps/stroy/models.py
+++ stroy_market/apps/stroy/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=125, verbose_name=_('Category name'))
+    image = models.ImageField(upload_to='category')
+    slug = models.SlugField(max_length=125, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
+        ordering = ('name',)
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=125, verbose_name=_('SubCategory name'))
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='category')
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = _('SubCategory')
+        verbose_name_plural = _('SubCategories')
+        ordering = ('name',)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(SubCategory, self).save(*args, **kwargs)
