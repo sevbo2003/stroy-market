122749a3b7434e79d52cb7e4f9fb113b852bea2f
Abdusamad Malikov <sevbofx@gmail.com>
2023-02-26 01:53:22 +0500

    `ProductFilter` implemented

--- stroy_market/apps/stroy/filters.py
+++ stroy_market/apps/stroy/filters.py
@@ -1,32 +1,47 @@
 from django_filters import rest_framework as filters
 from apps.stroy.models import Category, SubCategory, Product, ProductImage, Size, Color
-
+    
 
 class ProductFilter(filters.FilterSet):
     category = filters.CharFilter(field_name='category__category__slug')
     subcategory = filters.CharFilter(field_name='subcategory__slug')
-    view = filters.BooleanFilter(method='popular')
-    count = filters.BooleanFilter(method='latest')
+    popular = filters.BooleanFilter(method='filter_popular')
+    # rating = filters.BooleanFilter(method='rating', field_name='likes')
+    cheaper = filters.BooleanFilter(method='filter_cheaper')
+    expensive = filters.BooleanFilter(method='filter_expensive')
+    price_range = filters.RangeFilter(field_name='price')
+    in_discount = filters.BooleanFilter(field_name='in_discount')
 
     class Meta:
         model = Product
-        fields = ['category', 'subcategory', 'view', 'count']
+        fields = ['category', 'subcategory', 'popular', 'price_range', 'cheaper', 'expensive', 'in_discount']
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.filters['category'].label = 'Kategoriya'
         self.filters['subcategory'].label = 'Sub kategoriya'
-        self.filters['view'].label = 'Mashhur'
-        self.filters['count'].label = 'Recommended'
+        self.filters['popular'].label = 'Mashhur'
+        # self.filters['rating'].label = 'Rating'
+        self.filters['price_range'].label = 'Narx oraligi'
+        self.filters['cheaper'].label = 'Arzonlari oldin'
+        self.filters['expensive'].label = 'Qimmatlari oldin'
     
-    def popular(self, queryset, name, value):
+    def filter_popular(self, queryset, name, value):
         if value:
             return queryset.order_by('-views')
-        elif value == False:
-            return queryset.order_by('views')
         return queryset
 
-    def latest(self, queryset, name, value):
+    def filter_cheaper(self, queryset, name, value):
         if value:
-            return queryset.order_by('-created_at')
-        return queryset.order_by('created_at')
+            return queryset.order_by('price')
+        return queryset
+
+    def filter_expensive(self, queryset, name, value):
+        if value:
+            return queryset.order_by('-price')
+        return queryset
+    
+    # def latest(self, queryset, name, value):
+    #     if value:
+    #         return queryset.order_by('-created_at')
+    #     return queryset
