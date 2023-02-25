dba3d8833f25143c14106dbc80868e92a4dad619
Abdusamad Malikov <sevbofx@gmail.com>
2023-02-26 03:02:17 +0500

    `LikeComment`

--- stroy_market/apps/stroy/views.py
+++ stroy_market/apps/stroy/views.py
@@ -4,8 +4,8 @@ from rest_framework import permissions
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from apps.stroy.models import Category, SubCategory, Product, ProductImage, Size, Color, ProductComment
-from apps.stroy.serializers import CategorySerializer, SubCategorySerializer, ProductSerializer, ProductImageSerializer, ProductCommentSerializer
+from apps.stroy.models import Category, SubCategory, Product, ProductImage, Size, Color, ProductComment, CommentLike
+from apps.stroy.serializers import CategorySerializer, SubCategorySerializer, ProductSerializer, ProductImageSerializer, ProductCommentSerializer, CommentLikeSerializer
 from apps.stroy.filters import ProductFilter
 
 
@@ -97,5 +97,12 @@ class ProductViewSet(viewsets.ModelViewSet):
         pagination = self.paginate_queryset(queryset)
         if pagination is not None:
             return self.get_paginated_response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    @action(detail=True, methods=['post'])
+    def like_comment(self, request, pk=None):
+        serializer = CommentLikeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user, like=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
