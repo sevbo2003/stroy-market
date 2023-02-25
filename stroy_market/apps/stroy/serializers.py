fb156b204e4e92bf8ddbf877e0c38d115aef155d
Abdusamad Malikov <sevbofx@gmail.com>
2023-02-26 02:43:38 +0500

    `ProductCommentSerializer`

--- stroy_market/apps/stroy/serializers.py
+++ stroy_market/apps/stroy/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from apps.stroy.models import Category, SubCategory, Product, ProductImage, Size, Color
+from apps.stroy.models import Category, SubCategory, Product, ProductImage, Size, Color, ProductComment
+from django.conf import settings
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -15,9 +16,14 @@ class SubCategorySerializer(serializers.ModelSerializer):
 
 
 class ProductImageSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = ProductImage
         fields = ('id', 'product', 'image')
+    
+    def get_image(self, obj):
+        return settings.BASE_URL + obj.image.url
 
 
 class SizeSerializer(serializers.ModelSerializer):
@@ -40,13 +46,8 @@ class ProductSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Product
-        fields = ['id', 'name', 'price', 'price_with_discount', 'in_discount', 'discount_percent', 'category', 'count', 'weight', 'product_type', 'images', 'sizes', 'colors', 'control', 'purpose', 'material', 'xususiyatlari', 'brand', 'sotuvchi', 'description', 'views', 'like_count', 'created_at']
+        fields = ['id', 'name_uz', 'name_ru', 'price', 'price_with_discount', 'in_discount', 'discount_percent', 'category', 'count', 'weight', 'product_type', 'images', 'sizes', 'colors', 'control', 'purpose', 'material', 'xususiyatlari', 'brand', 'sotuvchi', 'description', 'views', 'avarage_rating', 'comments', 'created_at']
     
-    def to_representation(self, instance):
-        data = super().to_representation(instance)
-        data['likes'] = None
-        return data
-
     def get_images(self, obj):
         queryset = obj.productimage_set.all()
         serializer = ProductImageSerializer(queryset, many=True)
@@ -61,4 +62,16 @@ class ProductSerializer(serializers.ModelSerializer):
         queryset = obj.color_set.all()
         serializer = ColorSerializer(queryset, many=True)
         return serializer.data
+
+
+class ProductCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductComment
+        fields = ('id', 'product', 'user', 'comment', 'stars', 'created_at')
+        read_only_fields = ('user', 'created_at')
+    
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['user'] = instance.user.first_name + ' ' + instance.user.last_name
+        return data
     
