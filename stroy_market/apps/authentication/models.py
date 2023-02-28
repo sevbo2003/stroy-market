49d02dffe0d841edbb0791291ecb41c35364b6f2
Abdusamad Malikov <sevbofx@gmail.com>
2023-02-28 13:57:40 +0500

    `is_verified` field added

--- stroy_market/apps/authentication/models.py
+++ stroy_market/apps/authentication/models.py
@@ -24,6 +24,7 @@ class PhoneToken(models.Model):
     token = models.CharField(max_length=6, null=True, blank=True)
     created_at = models.DateTimeField(default=timezone.now, null=True, blank=True)
     expires_at = models.DateTimeField(null=True, blank=True)
+    is_verified = models.BooleanField(default=False)
 
     def __str__(self):
         return self.phone_number
