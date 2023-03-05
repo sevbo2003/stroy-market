552081bd972d4643e558dbb93d66f1d0a9a013c8
Abdusamad Malikov <sevbofx@gmail.com>
2023-03-06 00:35:59 +0500

    `PopularProduct` urls created

--- stroy_market/apps/stroy/urls.py
+++ stroy_market/apps/stroy/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from apps.stroy.views import CategoryViewSet, SubCategoryViewSet, ProductViewSet, CartItemViewSet, ProductLikeViewSet, BestProductsViewSet
+from apps.stroy.views import CategoryViewSet, SubCategoryViewSet, ProductViewSet, CartItemViewSet, ProductLikeViewSet, BestProductsViewSet, PopularProductsViewSet
 
 
 router = DefaultRouter()
 
 router.register("products", ProductViewSet)
 router.register("best-products", BestProductsViewSet)
+router.register("popular-products", PopularProductsViewSet)
 router.register("categories", CategoryViewSet)
 router.register("sub-categories", SubCategoryViewSet)
 router.register("cart-items", CartItemViewSet, basename='cart-items')
