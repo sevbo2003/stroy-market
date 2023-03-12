036edb9a48db35a0f269d90c11b011e8a7492f82
Abdusamad Malikov <sevbofx@gmail.com>
2023-03-12 14:46:20 +0500

    `asd`

--- stroy_market/apps/stroy/tasks.py
+++ stroy_market/apps/stroy/tasks.py
@@ -8,7 +8,6 @@ def send_news(message):
     try:
         for i in "stroy.models.Newsletter".objects.all():
             eskiz.send_sms(str(i)[1:], message, from_whom='4546')
-            return {"status": "success"}
     except Exception as e:
         return {"status": "error", "message": e}
     
