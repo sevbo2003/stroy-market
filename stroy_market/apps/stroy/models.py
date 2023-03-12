949acfd74bcd97fdfc12615b6834ad3db5240881
Abdusamad Malikov <sevbofx@gmail.com>
2023-03-12 13:16:20 +0500

    dadad

--- stroy_market/apps/stroy/models.py
+++ stroy_market/apps/stroy/models.py
@@ -252,4 +252,4 @@ class News(models.Model):
         super(News, self).save(*args, **kwargs)
         message = self.message
         numbers = Newsletter.objects.all().values_list('phone_number', flat=True)
-        send_news.apply_async(args=[message, numbers], countdown=10)
+        send_news.apply_async(args=[numbers, message], countdown=10)
