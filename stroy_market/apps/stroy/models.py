11f8207051eba62148e67f14cc38d12352da5307
Abdusamad Malikov <sevbofx@gmail.com>
2023-03-12 13:06:18 +0500

    `setted` to `phone_number`

--- stroy_market/apps/stroy/models.py
+++ stroy_market/apps/stroy/models.py
@@ -229,7 +229,7 @@ class Newsletter(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.email
+        return self.phone_number
 
     class Meta:
         verbose_name = _('Foydalanuvchi')
