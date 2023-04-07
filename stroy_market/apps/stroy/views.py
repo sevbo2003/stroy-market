e08b8a12d03f9ae78603a1557e6dc13cc44168e2
Abdusamad Malikov <sevbofx@gmail.com>
2023-04-07 09:46:33 +0500

    `remove-one`

--- stroy_market/apps/stroy/views.py
+++ stroy_market/apps/stroy/views.py
@@ -233,9 +233,9 @@ class CartItemViewSet(viewsets.ViewSet):
                 request.session.create()
                 session_key = request.session.session_key
             queryset = CartItem.objects.filter(session_key=request.session.session_key, id=pk)
-        if queryset.first().quantity == 1:
-            return Response(status=status.HTTP_204_NO_CONTENT)
         queryset.update(quantity=F('quantity') - 1)
+        if queryset[0].quantity == 0:
+            queryset.delete()
         return Response(status=status.HTTP_202_ACCEPTED)
 
     @action(detail=False, methods=['delete'])
