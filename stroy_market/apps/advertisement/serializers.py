3b74d793af4e7abfc6921b5622077bb8494121e1
Abdusamad Malikov <sevbofx@gmail.com>
2023-04-08 21:17:32 +0500

    `serializer` banner added

---
+++ stroy_market/apps/advertisement/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Banner
+
+
+class BannerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Banner
+        fields = '__all__'
+        
