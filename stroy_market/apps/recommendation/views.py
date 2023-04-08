7c474ac3c302c553790c069406ad74bb469a681f
Abdusamad Malikov <sevbofx@gmail.com>
2023-04-08 23:44:01 +0500

    `RecommendationViewSet`

--- stroy_market/apps/recommendation/views.py
+++ stroy_market/apps/recommendation/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import action
+from apps.recommendation.models import Recommendation, RecommendationProduct
+from apps.recommendation.serializers import RecommendationSerializer, RecommendationProductSerializer
 
-# Create your views here.
+
+class RecommendationViewSet(ReadOnlyModelViewSet):
+    queryset = Recommendation.objects.all()
+    serializer_class = RecommendationSerializer
+
+    @action(methods=['get'], detail=True)
+    def products(self, request, pk=None):
+        queryset = RecommendationProduct.objects.filter(recommendation_id=pk)
+        pagination = self.paginate_queryset(queryset)
+        if pagination is not None:
+            serializer = RecommendationProductSerializer(pagination, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = RecommendationProductSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
