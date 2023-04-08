128ff922d88089f9f92699ff44e169cd27c3be5e
Abdusamad Malikov <sevbofx@gmail.com>
2023-04-08 21:09:21 +0500

    `admin` banner added

--- stroy_market/apps/advertisement/admin.py
+++ stroy_market/apps/advertisement/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Banner
 
-# Register your models here.
+
+class BannerAdmin(admin.ModelAdmin):
+    list_display = ('image', 'is_active', 'width', 'height')
+    list_filter = ('is_active',)
+    list_display_links = ('image',)
+    list_editable = ('is_active', 'width', 'height')
+
+    
+
+
+admin.site.register(Banner, BannerAdmin)
