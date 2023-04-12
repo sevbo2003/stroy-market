f43c8743bc0e5276a6ec53db49fa6790515b5ea2
Abdusamad Malikov <sevbofx@gmail.com>
2023-04-12 20:08:51 +0500

    `RecommendationForProductDetail` viewsets urls

--- stroy_market/apps/recommendation/urls.py
+++ stroy_market/apps/recommendation/urls.py
@@ -1,13 +1,28 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from apps.recommendation.views import RecommendationViewSet, RecommentationForCartViewSet
+from apps.recommendation.views import (
+    RecommendationViewSet,
+    RecommentationForCartViewSet,
+    RecommendationForProductDetailViewSet,
+)
 
 router = DefaultRouter()
 
-router.register('home-page-recommends', RecommendationViewSet, basename='recommendation')
+router.register(
+    "home-page-recommends", RecommendationViewSet, basename="recommendation"
+)
+router.register(
+    "product-detail-recommends",
+    RecommendationForProductDetailViewSet,
+    basename="recommendation_for_product_detail",
+)
 
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('cart/', RecommentationForCartViewSet.as_view({'get': 'list'}), name='cart_recommendation'),
+    path("", include(router.urls)),
+    path(
+        "cart/",
+        RecommentationForCartViewSet.as_view({"get": "list"}),
+        name="cart_recommendation",
+    ),
 ]
