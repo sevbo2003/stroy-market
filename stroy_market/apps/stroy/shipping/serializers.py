5f7784e902acd228a7065d925051786be743553f
Abdusamad Malikov <sevbofx@gmail.com>
2023-04-24 19:56:00 +0500

    `domme`

--- stroy_market/apps/stroy/shipping/serializers.py
+++ stroy_market/apps/stroy/shipping/serializers.py
@@ -3,12 +3,15 @@ from apps.stroy.shipping.models import Order, OrderItem, OrderAddress
 from apps.stroy.models import PromoCode
 from django.contrib.auth import get_user_model
 from apps.stroy.models import CartItem
+from apps.stroy.serializers import ProductSerializer
 
 
 User = get_user_model()
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+    
     class Meta:
         model = OrderItem
         fields = ("id", "order", "product", "quantity", "price")
